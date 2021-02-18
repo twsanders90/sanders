fd4901edccdf166b222f6fdf257c1f6e033205f7
twsanders90 <twsanders90@neiu.edu>
2021-02-18 16:59:41 -0600

    Initial commit

---
+++ src/main/java/com/nofluffkitchen/sanders/SandersApplication.java
@@ -0,0 +1,13 @@
+package com.nofluffkitchen.sanders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nofluffkitchen/sanders/SandersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nofluffkitchen.sanders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
